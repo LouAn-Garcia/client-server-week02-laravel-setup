fa2b8af012fbaa363e9fe37e4d630ccc7ae0c9bb
LouAn-Garcia <garcialouanglie@gmail.com>
2026-08-07 23:50:50 +0800

    Initial Laravel setup

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Laravel Setup</title>
+</head>
+<body>
+    <h1>Student Information</h1>
+
+    <p><strong>Student Name:</strong> Lou Angeli L. Garcia</p>
+    <p><strong>Student Number:</strong> 0124-1380</p>
+    <p><strong>Course:</strong> BS Information Technology</p>
+    <p><strong>Section:</strong> BSIT 3A</p>
+    <p><strong>Subject:</strong> Client-Server Computing</p>
+</body>
+</html>
